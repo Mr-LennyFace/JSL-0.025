a4ab875435d2d5b5af8b0a5fd0e08c797c308909
Mr-LennyFace <explorist20@gmail.com>
2021-05-20 12:21:16 +0530

    2 files modified

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-5ggmqm
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5ggmqm)
--- index.js
+++ index.js
@@ -5,3 +5,27 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Open the Console</h1>`;
 
+function wordblanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = '';
+  result += 'The ' + myAdjective + myNoun + myVerb + myAdverb;
+
+  return result;
+}
+
+console.log(wordblanks('dog ', 'big ', 'ran ', 'quickly.'));
+
+var ARRAY = ['Sanjay', 12345];
+console.log(ARRAY);
+
+var array = [['Siddharth', 1234566], ['Mamma, 56543']];
+console.log(array);
+
+console.log(ARRAY[0]);
+console.log(ARRAY[1]);
+
+ARRAY[0] = 12354556;
+console.log(ARRAY[0]);
+console.log(ARRAY);
+var some = '';
+some = array[0][0];
+console.log(some);
